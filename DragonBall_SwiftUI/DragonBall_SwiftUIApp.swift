7dc2d8c9b58d2af06f2f194e08c5a54b2d5bd5ee
Lwis2804 <luis.enrique.enc@gmail.com>
2024-09-06 21:20:53 -0600

    Initial Commit

---
+++ DragonBall_SwiftUI/DragonBall_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBall_SwiftUIApp.swift
+//  DragonBall_SwiftUI
+//
+//  Created by LUIS GONZALEZ on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBall_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
