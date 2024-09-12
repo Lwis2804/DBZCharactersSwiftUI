570e0bd4da058c6bd837712d4226ecd3cef3045b
Lwis2804 <luis.enrique.enc@gmail.com>
2024-09-11 22:59:27 -0600

    [add] configuracion de vista de detalle

---
+++ Views/DBCharacterDetailView/DBCharacterDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DBCharacterDetailView.swift
+//  DragonBall_SwiftUI
+//
+//  Created by LUIS GONZALEZ on 11/09/24.
+//
+
+import SwiftUI
+
+struct DBCharacterDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DBCharacterDetailView()
+}
