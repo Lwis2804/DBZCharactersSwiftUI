3f2aa9d51b63d117217b14706c6e98c4803a995c
Lwis2804 <luis.enrique.enc@gmail.com>
2024-09-12 13:52:08 -0600

    [add] lista vista de gif

--- Views/GifWebView/GifWebView.swift
+++ Views/GifWebView/GifWebView.swift
@@ -5,4 +5,27 @@
 //  Created by LUIS GONZALEZ on 12/09/24.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct GifWebView: UIViewRepresentable {
+    let gifName: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.scrollView.isScrollEnabled = false
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url  = Bundle.main.url(forResource: gifName, withExtension: "gif") {
+            let request = URLRequest(url: url)
+            DispatchQueue.main.async {
+                uiView.load(request)
+            }
+        }
+    }
+ 
+}
+
+
